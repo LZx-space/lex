5991cf4bd2886daeea68f0ac244e4aff2b95b532
LZx <l.mikespace@outlook.com>
2025-11-22 19:33:12 +0800

    Implement buddy system frame structure
    Add linked list pointers to Frame struct for block management: -
    prev_block_head, self_block_head, and next_block_head - Include
    constructor method for initialization

--- src/memory/phys/frame.rs
+++ src/memory/phys/frame.rs
@@ -3,4 +3,25 @@ pub const FRAME_SIZE_BITS: usize = 12;
 /// 4KB
 pub const FRAME_SIZE: usize = 1 << FRAME_SIZE_BITS;
 
-pub struct Frame {}
+/// Represents a physical frame in memory.
+/// it must use the buddy system to manage memory. so add block info:
+/// - `prev_block_head`: Pointer to the previous block head frame in the same order.
+/// - `self_block_head`: Pointer to the current block head frame
+/// - `next_block_head`: Pointer to the next block head frame in the same order.
+pub struct Frame {
+    pfn: usize,
+    prev_block_head: Option<&'static Frame>,
+    self_block_head: Option<&'static Frame>,
+    next_block_head: Option<&'static Frame>,
+}
+
+impl Frame {
+    pub fn new(pfn: usize) -> Self {
+        Frame {
+            pfn,
+            prev_block_head: None,
+            self_block_head: None,
+            next_block_head: None,
+        }
+    }
+}
